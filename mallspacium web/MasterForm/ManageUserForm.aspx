ae4e628c1e8e6ce2bcc722a23627359a5e1cd85b
crowvenntheus <cabezasdeerickvenz07@gmail.com>
2023-02-15 22:00:05 +0800

    fixed gridview for the manage user form

--- mallspacium web/MasterForm/ManageUserForm.aspx
+++ mallspacium web/MasterForm/ManageUserForm.aspx
@@ -17,24 +17,24 @@
     </div>
      <div class="form">
 
-        <asp:GridView ID="manageUsersGridView" runat="server" HorizontalAlign="Center" AutoGenerateColumns="False" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" EditIndex="0">
-        <Columns>
+         <asp:GridView ID="manageUsersGridView" runat="server" HorizontalAlign="Center" AutoGenerateColumns="False" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3">
+             <Columns>
             <asp:BoundField HeaderText="User" DataField="manageUser"/>
             <asp:BoundField HeaderText="ID" DataField="manageId"/>
             <asp:BoundField HeaderText="Role" DataField="manageRole"/>
             <asp:BoundField HeaderText="Date" DataField="manageDate"/>
             <asp:BoundField HeaderText="Status" DataField="manageStatus"/>
         </Columns>
-                <FooterStyle BackColor="White" ForeColor="#000066" />
-                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
-                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
-                <RowStyle ForeColor="#000066" />
-                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
-                <SortedAscendingCellStyle BackColor="#F1F1F1" />
-                <SortedAscendingHeaderStyle BackColor="#007DBB" />
-                <SortedDescendingCellStyle BackColor="#CAC9C9" />
-                <SortedDescendingHeaderStyle BackColor="#00547E" />
-    </asp:GridView>
+             <FooterStyle BackColor="White" ForeColor="#000066" />
+             <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+             <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+             <RowStyle ForeColor="#000066" />
+             <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+             <SortedAscendingCellStyle BackColor="#F1F1F1" />
+             <SortedAscendingHeaderStyle BackColor="#007DBB" />
+             <SortedDescendingCellStyle BackColor="#CAC9C9" />
+             <SortedDescendingHeaderStyle BackColor="#00547E" />
+         </asp:GridView>
     </div>
    
         </div>
--- mallspacium web/MasterForm/ManageUserForm.aspx.cs
+++ mallspacium web/MasterForm/ManageUserForm.aspx.cs
@@ -12,44 +12,42 @@ namespace mallspacium_web
     public partial class WebForm1 : System.Web.UI.Page
     {
         FirestoreDb database;
+        DataTable usersGridViewTable = new DataTable();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             string path = AppDomain.CurrentDomain.BaseDirectory + @"mallspaceium.json";
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
             database = FirestoreDb.Create("mallspaceium");
+     
+            usersGridViewTable.Columns.Add("manageUser");
+            usersGridViewTable.Columns.Add("manageId");
+            usersGridViewTable.Columns.Add("manageRole");
+            usersGridViewTable.Columns.Add("manageDate");
+            usersGridViewTable.Columns.Add("manageStatus");
 
             getManageUsers("AdminManageUsers");
-
         }
         public async void getManageUsers(string AdminManageUsers)
         {
             Query usersQue = database.Collection(AdminManageUsers);
             QuerySnapshot snap = await usersQue.GetSnapshotAsync();
 
+            
+
             foreach (DocumentSnapshot docsnap in snap.Documents)
             {
                 ManageUsers user = docsnap.ConvertTo<ManageUsers>();
 
                 if (docsnap.Exists)
-                {
-                    DataTable usersGridViewTable = new DataTable();
-                    usersGridViewTable.Columns.Add("manageUser");
-                    usersGridViewTable.Columns.Add("manageId");
-                    usersGridViewTable.Columns.Add("manageRole");
-                    usersGridViewTable.Columns.Add("manageDate");
-                    usersGridViewTable.Columns.Add("manageStatus");
-
-                    usersGridViewTable.Rows.Add(user.manageUser, user.manageId, user.manageRole, user.manageDate, user.manageStatus);
-                    manageUsersGridView.DataSource = usersGridViewTable;
-                    manageUsersGridView.DataBind();
-                }
-                else
-                {
-                    Response.Write("<script>alert('No Record!');</script>");
+                {              
+                    usersGridViewTable.Rows.Add(user.manageUser, user.manageId, user.manageRole, user.manageDate, user.manageStatus);                  
                 }
             }
-            
+            manageUsersGridView.DataSource = usersGridViewTable;
+            manageUsersGridView.DataBind();
+
         }       
     }
 }
