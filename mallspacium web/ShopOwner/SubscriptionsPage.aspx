9d259a75ba6887adfad372f702a637672406137b
crowvenntheus <cabezasdeerickvenz07@gmail.com>
2023-03-21 22:40:46 +0800

    subscription page for both shopper and shop owner

---
+++ mallspacium web/ShopOwner/SubscriptionsPage.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SubscriptionsPage.aspx.cs" Inherits="mallspacium_web.ShopOwner.SubscriptionsPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="subscription-choice">
+  <h2>
+      <asp:Label ID="BasicSubscriptionLabel" runat="server" Text="Basic Subscription"></asp:Label>
+            </h2>
+  <p>₱<asp:Label ID="BasicSubPriceLabel" runat="server" Text="149.80"></asp:Label>
+&nbsp;per month</p>
+  <ul>
+    <li>Access to basic content</li>
+    <li>Monthly newsletter</li>
+  </ul>
+            <asp:Button ID="BasicSubButton" runat="server" OnClick="BasicSubButton_Click" Text="Subscribe" />
+</div>
+
+<div class="subscription-choice">
+  <h2>
+      <asp:Label ID="AdvancedSubscriptionLabel" runat="server" Text="Advanced Subscription"></asp:Label>
+    </h2>
+  <p>₱<asp:Label ID="AdvancedSubPriceLabel" runat="server" Text="299.06"></asp:Label>
+&nbsp;per month</p>
+  <ul>
+    <li>Access to advanced content</li>
+    <li>Weekly newsletter</li>
+    <li>Exclusive offers</li>
+  </ul>
+    <asp:Button ID="AdvanceSubButton" runat="server" OnClick="AdvanceSubButton_Click" Text="Subscribe" />
+</div>
+
+<div class="subscription-choice">
+  <h2>
+      <asp:Label ID="PremiumSubscriptionLabel" runat="server" Text="Premium Subscription"></asp:Label>
+    </h2>
+    <p>
+        ₱<asp:Label ID="PremiumSubPriceLabel" runat="server" Text="449.04"></asp:Label>
+&nbsp;per month</p>
+    <ul>
+        <li>Access to premium contently newsletter</li>
+    <li>Exclusive offers</li>
+    <li>Priority support</li>
+  </ul>
+    <asp:Button ID="PremiumSubButton" runat="server" OnClick="PremiumSubButton_Click" Text="Subscribe" />
+</div>
+    </form>
+</body>
+</html>
---
+++ mallspacium web/ShopOwner/SubscriptionsPage.aspx.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Google.Cloud.Firestore;
+
+namespace mallspacium_web.ShopOwner
+{
+    public partial class SubscriptionsPage : System.Web.UI.Page
+    {
+        FirestoreDb db;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"mallspaceium.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            db = FirestoreDb.Create("mallspaceium");
+        }
+
+        protected void BasicSubButton_Click(object sender, EventArgs e)
+        {
+            basicSubscription();
+        }
+
+        protected void AdvanceSubButton_Click(object sender, EventArgs e)
+        {
+            advancedSubscription();
+        }
+
+        protected void PremiumSubButton_Click(object sender, EventArgs e)
+        {
+            premiumSubscription();
+        }
+
+        public async void basicSubscription()
+        {
+            String status = "Active";
+            // Query the Firestore collection for a user with a specific email address
+            CollectionReference usersRef = db.Collection("Users");
+            DocumentReference docRef = usersRef.Document((string)Application.Get("usernameget"));
+
+            // Retrieve the document data asynchronously
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+            // Check if the document exists
+            if (snapshot.Exists)
+            {
+                // Get the data as a Dictionary
+                Dictionary<string, object> data = snapshot.ToDictionary();
+                // Access the specific field you want
+                string userEmail = data["email"].ToString();
+                string userRole = data["userRole"].ToString();
+
+                // Do something with the field value
+
+                // Generate random ID number
+                Random random = new Random();
+                int randomIDNumber = random.Next(100000, 999999);
+                string subscriptionID = "SUB" + randomIDNumber.ToString();
+
+                // Get current date time and the expected expiration date
+                DateTime currentDate = DateTime.Now;
+                DateTime expirationDate = currentDate.AddMonths(3);
+                string startDate = currentDate.ToString("yyyy-MM-dd HH:mm:ss");
+                string endDate = expirationDate.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+                // Create a new collection reference
+                DocumentReference documentRef = db.Collection("AdminManageSubscription").Document(userEmail);
+
+                // Set the data for the new document
+                Dictionary<string, object> dataInsert = new Dictionary<string, object>
+                {
+                    {"subscriptionID", subscriptionID},
+                    {"subscriptionType", BasicSubscriptionLabel.Text.ToString()},
+                    {"price", BasicSubPriceLabel.Text.ToString()},
+                    {"userEmail", userEmail},
+                    {"userRole", userRole},
+                    {"startDate", startDate},
+                    {"endDate", endDate},
+                    {"status", status}
+                };
+
+                // Set the data in the Firestore document
+                await documentRef.SetAsync(dataInsert);
+            }
+            else
+            {
+                // Document does not exist
+            }
+        }
+
+        public async void advancedSubscription()
+        {
+            String status = "Active";
+            // Query the Firestore collection for a user with a specific email address
+            CollectionReference usersRef = db.Collection("Users");
+            DocumentReference docRef = usersRef.Document((string)Application.Get("usernameget"));
+
+            // Retrieve the document data asynchronously
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+            // Check if the document exists
+            if (snapshot.Exists)
+            {
+                // Get the data as a Dictionary
+                Dictionary<string, object> data = snapshot.ToDictionary();
+                // Access the specific field you want
+                string userEmail = data["email"].ToString();
+                string userRole = data["userRole"].ToString();
+
+                // Do something with the field value
+
+                // Generate random ID number
+                Random random = new Random();
+                int randomIDNumber = random.Next(100000, 999999);
+                string subscriptionID = "SUB" + randomIDNumber.ToString();
+
+                // Get current date time and the expected expiration date
+                DateTime currentDate = DateTime.Now;
+                DateTime expirationDate = currentDate.AddMonths(3);
+                string startDate = currentDate.ToString("yyyy-MM-dd HH:mm:ss");
+                string endDate = expirationDate.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+                // Create a new collection reference
+                DocumentReference documentRef = db.Collection("AdminManageSubscription").Document(userEmail);
+
+                // Set the data for the new document
+                Dictionary<string, object> dataInsert = new Dictionary<string, object>
+                {
+                    {"subscriptionID", subscriptionID},
+                    {"subscriptionType", BasicSubscriptionLabel.Text.ToString()},
+                    {"price", BasicSubPriceLabel.Text.ToString()},
+                    {"userEmail", userEmail},
+                    {"userRole", userRole},
+                    {"startDate", startDate},
+                    {"endDate", endDate},
+                    {"status", status}
+                };
+
+                // Set the data in the Firestore document
+                await documentRef.SetAsync(dataInsert);
+            }
+            else
+            {
+                // Document does not exist
+            }
+        }
+
+        public async void premiumSubscription()
+        {
+            String status = "Active";
+            // Query the Firestore collection for a user with a specific email address
+            CollectionReference usersRef = db.Collection("Users");
+            DocumentReference docRef = usersRef.Document((string)Application.Get("usernameget"));
+
+            // Retrieve the document data asynchronously
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+            // Check if the document exists
+            if (snapshot.Exists)
+            {
+                // Get the data as a Dictionary
+                Dictionary<string, object> data = snapshot.ToDictionary();
+                // Access the specific field you want
+                string userEmail = data["email"].ToString();
+                string userRole = data["userRole"].ToString();
+
+                // Do something with the field value
+
+                // Generate random ID number
+                Random random = new Random();
+                int randomIDNumber = random.Next(100000, 999999);
+                string subscriptionID = "SUB" + randomIDNumber.ToString();
+
+                // Get current date time and the expected expiration date
+                DateTime currentDate = DateTime.Now;
+                DateTime expirationDate = currentDate.AddMonths(3);
+                string startDate = currentDate.ToString("yyyy-MM-dd HH:mm:ss");
+                string endDate = expirationDate.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+                // Create a new collection reference
+                DocumentReference documentRef = db.Collection("AdminManageSubscription").Document(userEmail);
+
+                // Set the data for the new document
+                Dictionary<string, object> dataInsert = new Dictionary<string, object>
+                {
+                    {"subscriptionID", subscriptionID},
+                    {"subscriptionType", BasicSubscriptionLabel.Text.ToString()},
+                    {"price", BasicSubPriceLabel.Text.ToString()},
+                    {"userEmail", userEmail},
+                    {"userRole", userRole},
+                    {"startDate", startDate},
+                    {"endDate", endDate},
+                    {"status", status}
+                };
+
+                // Set the data in the Firestore document
+                await documentRef.SetAsync(dataInsert);
+            }
+            else
+            {
+                // Document does not exist
+            }
+        }
+    }
+}
