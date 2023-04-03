7a8e36943a97031d63b94bbc510a1b24ec48d1b7
ChristMagallon <chanblade123@gmail.com>
2023-04-03 20:37:04 +0800

    working notification

--- mallspacium web/ShopOwner/NotificationPage.aspx.designer.cs
+++ mallspacium web/ShopOwner/NotificationPage.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace mallspacium_web.MasterForm2
     {
 
         /// <summary>
-        /// searchTextBox control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox searchTextBox;
-
-        /// <summary>
         /// NotificationGridView control.
         /// </summary>
         /// <remarks>
